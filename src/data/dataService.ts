5dfee8363b0173639dec94c41f512e88f8f11887
Martin Alvarez <martinalvarez@gmail.com>
2023-11-12 22:13:18 -0300

    Decrease delay simulated time fetching data

--- src/data/dataService.ts
+++ src/data/dataService.ts
@@ -1,7 +1,7 @@
 import { Db } from "../types/Db";
 
 export async function fetchDb() {
-  await new Promise(resolve => setTimeout(resolve, 1000));
+  await new Promise(resolve => setTimeout(resolve, 500));
 
   const response = await fetch('./data.json');
   const data: Db = await response.json();
